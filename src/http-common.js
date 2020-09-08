4098cb822cd229d0cbe925ba1592399f4f27b9a9
Rafael Alves <rafaelvitor_as@hotmail.com>
2020-09-07 22:18:31 -0300

    Update http-common.js

--- src/http-common.js
+++ src/http-common.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-  baseURL: 'https://grades-api-rv.herokuapp.com/:8080/',
+  baseURL: 'https://grades-api-rv.herokuapp.com:8080',
   headers: {
     'Content-type': 'application/json',
   },
